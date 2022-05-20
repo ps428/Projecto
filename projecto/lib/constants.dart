2477ef3d1524f429480ea2b2ba18da51b3cda29b
Pranav <pranav.bhawan@gmail.com>
2022-05-20 22:26:50 +0530

    Firebase setup, added basic UI and constant for custom text and some colours

--- projecto/lib/constants.dart
+++ projecto/lib/constants.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
+const mustard = Color(0xffffdb58);
 const japanBlush = Color(0xffE2D6F3);
+const cavernPink = Color(0xffE2BCB7);
+const parchmentPaper = Color(0xffF0E5D8);
+const babyPink = Color(0xffFCD1D1);
+const wildWatermelon = Color(0xffFF577F);
+const reddish = Color(0xffE10032);
+const brown = Color(0xff5D373D);
+const monteCarlo = Color(0xffABD8D8);
+const coriander = Color(0xffC1DEAE);
+const militantVegan = Color(0xff219F94);
+const pineGreen = Color(0xff15717E);
+const bottleGreen2 = Color(0xff006A4E);
+const stromboli = Color(0xff2E5E4E);
+const bottleGreen = Color(0xff072227);
 
-class Header extends StatelessWidget {
-  const Header(this.heading);
+class CustomText extends StatelessWidget {
+  const CustomText(this.heading, this.size, this.color);
+  final double size;
+  final Color color;
   final String heading;
 
   @override
@@ -11,7 +27,8 @@ class Header extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Text(
           heading,
-          style: const TextStyle(fontSize: 24, fontFamily: 'Playfair'),
+          style:
+              TextStyle(fontSize: size, fontFamily: 'Playfair', color: color),
         ),
       );
 }
